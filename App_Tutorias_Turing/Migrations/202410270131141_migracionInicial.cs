0755c7fbe9a4323141b6bd3542ab096bab63c59b
drodriguezalas <drodriguezalas@gmail.com>
2024-10-26 19:31:54 -0600

    Agregar migración de datos al repo

---
+++ App_Tutorias_Turing/Migrations/202410270131141_migracionInicial.cs
@@ -0,0 +1,16 @@
+﻿namespace App_Tutorias_Turing.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class migracionInicial : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
