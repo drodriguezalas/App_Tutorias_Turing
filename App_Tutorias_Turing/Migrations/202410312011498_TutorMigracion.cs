60e649242db6302c774a017c39d5dc5b63fa4c7e
Xiao-Chen-GG <binxiao15@gmail.com>
2024-10-31 14:20:42 -0600

    Vista-de-matricula

---
+++ App_Tutorias_Turing/Migrations/202410312011498_TutorMigracion.cs
@@ -0,0 +1,27 @@
+﻿namespace App_Tutorias_Turing.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TutorMigracion : DbMigration
+    {
+        public override void Up()
+        {
+            DropTable("dbo.Matriculas");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.Matriculas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UsuarioId = c.Int(nullable: false),
+                        TutoriaId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+    }
+}
